e30d92f9343f2236b18eff51ff20e48e7749a778
yunhaoc <41235838+yunhaoc@users.noreply.github.com>
2018-07-16 10:15:07 +0800

    Add files via upload
    修改word表格内容填写不完整的bug;解决内容为整型数据时，写入word文档后带小数点的问题

--- src/execlToDoc1.0.py
+++ src/execlToDoc1.0.py
@@ -13,6 +13,7 @@ document = Document()
 
 #计算execl中sheet的个数
 sheetCount = len(execl.sheets())
+#print(sheetCount)
 for index in range(0,sheetCount):
     table = execl.sheets()[index]
     #获取表格的行数和列数
@@ -30,15 +31,14 @@ for index in range(0,sheetCount):
         #因为因为字典d 是一个object ,而mergedInfo.append(mergedDict)并没有真正的将该字典在内存中再次创建。只是指向了相同的object。
         #这也是python 提高性能，优化内存的考虑。
         mergedInfo.append(temp)
+        
     i = 0
     writeRe = 0
     while i < tableRows:
         isExist = False
         if len(mergedInfo) > 0:
             for j in range(0,len(mergedInfo)):
-                print(mergedInfo[j]['rs'])
                 if i == mergedInfo[j]['rs']:
-                    print("hell")
                     mergeRow = mergedInfo[j]['re'] - mergedInfo[j]['rs']
                     isExist = True
                     break
@@ -48,16 +48,26 @@ for index in range(0,sheetCount):
         writeRe = i + mergeRow 
         wordTable = document.add_table(rows=mergeRow, cols=tableCols - 1,style="Table Grid")
         document.add_paragraph()
-        
+  
+        #每个表格开始写的时候行索引都为0
         rowIndex = 0
-        colIndex = 0
         #向表格写数据 
         for row in range(i,writeRe):
+            #每行开始写数据时，列索引都为0
+            colIndex = 0
             for col in range(1,tableCols):
                 data = table.cell(row,col)
                 if data.value is not None:
                     try:
-                        wordTable.cell(rowIndex,colIndex).text = str(data.value)
+                        writeTableData = str("")
+                        if isinstance(data.value,float):
+                            if  round(data.value) == data.value:
+                                writeTableData = str(round(data.value))
+                            else:
+                                writeTableData = str(data.value)
+                        else:
+                            writeTableData = data.value
+                        wordTable.cell(rowIndex,colIndex).text = str(writeTableData)
                         colIndex = colIndex + 1
                     except IndexError:
                         pass
